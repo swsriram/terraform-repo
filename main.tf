b97562fe13d60463baf3fd0a479d9e1c1ce7cc21
swsriram <sk.swaroop@gmail.com>
2025-01-22 11:51:51 +0530

    Update main.tf
    changing the availability zone from sa-east-1b to sa-east-1c to overcome the error:
    [31m│[0m [0m[1m[31mError: [0m[0m[1mcreating EC2 Instance: operation error EC2: RunInstances, https response error StatusCode: 400, RequestID: 02762c3d-6e06-429e-ab80-d38ba55721a9, api error Unsupported: Your requested instance type (t2.micro) is not supported in your requested Availability Zone (sa-east-1b). Please retry your request by not specifying an Availability Zone or choosing sa-east-1a, sa-east-1c.

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_subnet_cidr
-  availability_zone = "${var.aws_region}b"
+  availability_zone = "${var.aws_region}c"
   tags = {
     Name = "PrivateSubnet"
   }
